bbd67b098ef50bd5ebc63245f35c4e5cbb561bcd
François KERISIT <FanchGadjo@users.noreply.github.com>
2023-04-06 11:19:34 +0200

    feat: add "prefer collections with pagination"

---
+++ src/prefer-collections-with-pagination.ts
@@ -0,0 +1,36 @@
+import {
+  Controller, Get
+} from "@nestjs/common";
+
+interface Pagination {
+  items: string[];
+  currentPage: number;
+  totalPages: number;
+}
+
+@Controller("cats")
+export class CatsController {
+  //NOK: raw collection is not paginated
+  @Get("raw")
+  getRawCollection(): Promise<string[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(["Siamese", "Maine Coon", "Chartreux"]);
+      }, 300);
+    });
+  }
+
+  //OK: collection is paginated
+  @Get("page")
+  getPaginatedCollection(): Promise<Pagination> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          items: ["Siamese", "Maine Coon", "Chartreux"],
+          currentPage: 0,
+          totalPages: 1,
+        });
+      }, 300);
+    });
+  }
+}
