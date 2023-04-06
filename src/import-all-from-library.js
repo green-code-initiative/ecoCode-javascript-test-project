9a5499ba4db3f8f5d38d0fd21b6869bb011f7e79
Basnier Julien <julien.basnier-ext@natixis.com>
2023-04-06 13:24:51 +0200

    no-no-import-all-from-library rule example

---
+++ src/import-all-from-library.js
@@ -0,0 +1,10 @@
+import lodash from 'lodash';
+import * as _ from 'underscore';
+function invalidImport() {
+    const isEmpty = lodash.isEmpty('');
+    const uderscoreTest = _.isEmpty('');
+}
+
+
+
+
