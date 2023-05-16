ae52c2d362a7f6de9b4041a1f3b89270e8137ba5
David DE CARVALHO <dedece35@gmail.com>
2023-05-16 16:32:46 +0200

    add tool script to send to sonarqube - update comments

--- tool_send_to_sonar.sh
+++ tool_send_to_sonar.sh
@@ -3,6 +3,14 @@
 # "sonar.login" variable : private TOKEN generated in your local SonarQube during installation
 # (input paramater of this script)
 
+# requirements : yarn installed with 
+# "npm instal yarn"
+
+# building phase
 yarn install
+
+# analysis phase
 yarn lint:report
+
+# sending to Sonar phase
 yarn sonar -Dsonar.login=$1
