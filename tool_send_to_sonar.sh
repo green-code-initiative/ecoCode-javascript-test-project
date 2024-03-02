8e42dd39b6f7fd8485805a028dbfb4a99b156e2e
utarwyn <maxime.malgorn@laposte.net>
2024-03-02 15:38:42 +0100

    Bump dependencies

--- tool_send_to_sonar.sh
+++ tool_send_to_sonar.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-# "sonar.login" variable : private TOKEN generated in your local SonarQube during installation
+# "sonar.token" variable : private TOKEN generated in your local SonarQube during installation
 # (input paramater of this script)
 
 # building phase
 yarn install
 
 # sending to Sonar phase
-yarn sonar -Dsonar.login=$1
+yarn sonar -Dsonar.token=$1
