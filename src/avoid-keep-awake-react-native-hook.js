b8b3ba8fcf7cec6810d8eaa49a9cd1898e189fe8
François KERISIT <f.kerisit@gmail.com>
2024-05-30 13:45:21 +0200

    add avoid keep awake examples

---
+++ src/avoid-keep-awake-react-native-hook.js
@@ -0,0 +1,13 @@
+import { useKeepAwake } from 'expo-keep-awake';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function KeepAwakeExample() {
+  // Non-compliant
+  useKeepAwake();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>This screen will never sleep!</Text>
+    </View>
+  );
+}
