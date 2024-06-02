8876b6b26b863ead9e4a045dbae2b792920a92cf
chihebTalan <148899569+chihebtalan@users.noreply.github.com>
2024-06-02 18:40:25 +0200

    Update tool_send_to_sonar.sh

--- tool_send_to_sonar.sh
+++ tool_send_to_sonar.sh
@@ -7,5 +7,4 @@
 yarn install
 
 # sending to Sonar phase
-#yarn sonar:sonar -Dsonar.host.url=http://localhost:8000 -Dsonar.login=admin -Dsonar.password=chiheb
-yarn sonar -Dsonar.token=sqa_f3ffd2ce15ddac2783f2bd2c01aac927e7a27656
+yarn sonar -Dsonar.token=$1
